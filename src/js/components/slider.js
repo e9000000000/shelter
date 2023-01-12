7c3f5cfecca7da2b824b83ae1d7a68562bc79d5a
e9000000000 <skipper224483@gmail.com>
2023-01-12 20:01:41 +0300

    adaptive swiper

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,10 +1,28 @@
 import Swiper, { Navigation, Scrollbar } from "swiper";
 Swiper.use([Navigation, Scrollbar]);
+
+function getPerView() {
+  const width = Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  )
+  return width < 600 ? 1 : width < 900 ? 2 : 3
+}
+
+
 const swiper = new Swiper(".our__content", {
-  slidesPerView: 3,
+  slidesPerView: getPerView(),
   spaceBetween: 90,
   navigation: {
     nextEl: ".our__slider-btn--next",
     prevEl: ".our__slider-btn--prev",
   },
 });
+
+
+addEventListener("resize", () => {
+  swiper.params.slidesPerView = getPerView()
+})
